0a3e79bb2ee6bd1381de1fd573d7337ffa2a60bb
Calvin <31526471+Om-Pandey@users.noreply.github.com>
2018-06-07 02:05:14 +0530

    Added final student log

--- waves.js
+++ waves.js
@@ -598,3 +598,16 @@
 
     return Waves;
 });
+ $(function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 2000,
+      values: [ 0, 1500 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
